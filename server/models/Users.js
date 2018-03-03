b5c87c6f40aa347ed5d1cc61f01685c0bc7bbfa4
Mohamed abdelazem <m.abdelazem@hotmail.com>
2018-03-03 21:22:03 +0200

    login

--- server/models/Users.js
+++ server/models/Users.js
@@ -53,6 +53,25 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email,password) {
+        var User= this;
+
+    return User.findOne({email}).then((user)=>{
+      if(!user){
+        return Promise.reject();
+      }
+          return new Promise((resolve,reject)=>{
+            bcrypt.compare(password,user.password,(err,res)=>{
+              if(res){
+                resolve(user);
+              }else{
+                reject();
+              }
+            });
+          });
+    });
+};
+
 UserSchema.statics.findByToken = function(token) {
   var User =  this;
   var decoded;
@@ -74,7 +93,7 @@ UserSchema.pre('save',function(next){
   if (user.isModified('password')){
      bcrypt.genSalt(10,(err,salt)=>{
         bcrypt.hash(user.password,salt,(err,hash)=>{
-          user.password = hash;  
+          user.password = hash;
           next();
       });
     });
@@ -82,6 +101,8 @@ UserSchema.pre('save',function(next){
     next();
   }
 });
+
+
 
 var User =  mongoose.model('User',UserSchema);
 
